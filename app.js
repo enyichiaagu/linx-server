cee992d24f95118b5ae6ae9ab11e1a43a47b4aee
enyichiaagu <enyichiaagu@gmail.com>
2021-09-03 19:00:11 +0100

    push

--- app.js
+++ app.js
@@ -19,6 +19,13 @@ mongoose.connect(
 //Middlewares
 app.use(express.json())
 app.use(cors())
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
 
 app.get('/', (req, res) => {
     res.send("<h1>Hello World. This is Linx - The server</h1>")
