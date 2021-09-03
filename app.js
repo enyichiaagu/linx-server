0fbd0dd1566fb3a719cb6bb335af4e88cb0f5b4f
enyichiaagu <enyichiaagu@gmail.com>
2021-09-03 19:12:26 +0100

    push

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(function (req, res, next) {
 });
 
 app.get('/', (req, res) => {
-    res.sendFile('<h1>Is this even working?</h1>')
+    res.send('<h1>Is this even working?</h1>')
 })
 
 //Route Middlewares
